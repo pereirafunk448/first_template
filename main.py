5bd70e1cb6407caa9fe90cb2930cf218e088cc37
vinicius almeida <87548413+pereirafunk448@users.noreply.github.com>
2022-03-17 22:37:40 +0000

    coloquei template

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
-  return '<h1>Minha primeira rota!</h1>'
-
-
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
